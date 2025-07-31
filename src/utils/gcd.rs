863f94de3b9876843f528c361f43934a28789fd6
parsasarfarazi <parsasarfarazi@gmail.com>
2025-07-31 18:23:04 +0330

    #1 Fix(utils::gcd) : gcd function now takes slices of array

--- src/utils/gcd.rs
+++ src/utils/gcd.rs
@@ -1,6 +1,6 @@
 /// Calculates the greatest common divisors of a vector of numbers using the euclidian algorithm
 /// #### Arguments
-/// * `numbers` - A vector of unsigned 32-bit integer
+/// * `numbers` - A reference to a vector of unsigned 32-bit integers
 /// #### Returns
 /// * `0` if the input is empty
 /// * the gcd of the numbers
@@ -9,19 +9,19 @@
 /// use crypto_toolbox::utils::gcd;
 ///
 /// let numbers = vec![12, 18, 24];
-/// assert_eq!(gcd::gcd(numbers), 6);
+/// assert_eq!(gcd::gcd(&numbers), 6);
 ///
 /// let empty: Vec<u32> = vec![];
-/// assert_eq!(gcd::gcd(empty), 0);
+/// assert_eq!(gcd::gcd(&empty), 0);
 /// ```
-pub fn gcd(numbers: Vec<u32>) -> u32 {
+pub fn gcd(numbers: &[u32]) -> u32 {
     if numbers.is_empty() {
         return 0;
     }
     if numbers.len() == 1 {
         return numbers[0];
     }
-    let mut result: u32 = numbers[0];
+    let mut result = numbers[0];
     for &number in numbers.iter().skip(1) {
         result = gcd_of_two(result, number);
         if result == 1 {
@@ -31,7 +31,7 @@ pub fn gcd(numbers: Vec<u32>) -> u32 {
     result
 }
 
-/// core logic of the gcd eulidian algorithm for only two numbers
+/// Core logic of the GCD Euclidean algorithm for only two numbers
 fn gcd_of_two(r0: u32, r1: u32) -> u32 {
     if r1 == 0 { r0 } else { gcd_of_two(r1, r0 % r1) }
 }
@@ -45,7 +45,7 @@ mod test {
     #[test]
     fn test_gcd() {
         let numbers: Vec<u32> = vec![5, 10, 15];
-        let g = gcd(numbers);
+        let g = gcd(&numbers);
         assert_eq!(g, 5)
     }
 }
